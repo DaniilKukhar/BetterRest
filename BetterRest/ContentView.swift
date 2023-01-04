f8242b37e45f5e6a147163a7cee4db5341e7ad96
Daniil Kukhar <82108121+DaniilKukhar@users.noreply.github.com>
2023-01-04 17:55:21 +0200

    Add mlmodel, labels, date picker, steppers

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,12 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("What do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            }
+            .navigationTitle("Better Rest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
         }
         .padding()
+    }
+    
+    func calculateBedTime() {
+        
     }
 }
 
